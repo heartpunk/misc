90fdd0905ddd6b46ededc05f89eaa002c3c15667
Ezekiel Smithburg <tehgeekmeister@gmail.com>
2013-01-02 09:34:53 -0800

    kill the warmers when you exit the script.

--- warmth.rb
+++ warmth.rb
@@ -1,10 +1,14 @@
-script = <<SCRIPT
+begin
+  script = <<SCRIPT
 while true do
   true
 end
 SCRIPT
 
-individual_command = 'ruby -e "' + script + '"'
-group_command = (1..8).to_a.collect {individual_command}.join("&")
+  individual_command = 'ruby -e "' + script + '"'
+  group_command = (1..32).to_a.collect {individual_command}.join("&")
 
-`#{group_command}`
+  `#{group_command}`
+rescue SystemExit, Interrupt
+  `ps aux | grep ruby | grep while | cut -d' ' -f 2 | xargs kill`
+end
