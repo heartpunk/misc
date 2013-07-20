4ebd37a5d8a4f8c0e00324d731e26ddca40b126a
Ezekiel Smithburg <tehgeekmeister@gmail.com>
2013-07-20 03:01:59 -0700

    some primes.rb fixes that were hanging out. oopsie.

--- primes.rb
+++ primes.rb
@@ -1,16 +1,19 @@
 def primes upto=1000
   cache = [2,3]
   offset = 1
-  while cache[-1] < upto
+  upto_sqrt = Math.sqrt upto
+  last = -2
+  while last < upto_sqrt
     possible_prime = cache[-1] + offset
-    if cache.none? {|i| possible_prime % i == 0}
+    if cache.take_while {|el| upto_sqrt > el}.none? {|i| possible_prime % i == 0}
       offset = 0
       cache << possible_prime
+      yield possible_prime if block_given?
     else
       offset += 1
     end
+    last = cache[-1]
   end
-  cache
 end
 
-puts primes 10_000
+primes(10_000_000_000) {|p| puts p.to_s.reverse.gsub(/...(?=.)/,'\&,').reverse}
