5e645c7b3f902c2648a117af71b662cff77b8192
Ezekiel Smithburg <tehgeekmeister@gmail.com>
2013-07-20 15:44:59 -0700

    significant simplification of conditional probability example.

--- probability.jl
+++ probability.jl
@@ -1,20 +1,8 @@
 function P_h2 (P_h_coin_a, P_h_coin_b_given_h_coin_a, P_t_coin_c_given_t_coin_a)
   denom, num = 0
 
-  tmp = P_h_coin_a * P_h_coin_b_given_h_coin_a
-  denom += tmp
-  num += tmp
-
-  denom += P_h_coin_a * inverse_p(P_h_coin_b_given_h_coin_a)
-
-  P_t_coin_a = inverse_p(P_h_coin_a)
-
-  tmp = P_t_coin_a * P_t_coin_c_given_t_coin_a
-  denom += tmp
-
-  tmp = P_t_coin_a * inverse_p(P_t_coin_c_given_t_coin_a)
-  denom += tmp
-  num += tmp
+  num = P_h_coin_a * P_h_coin_b_given_h_coin_a + inverse_p(P_h_coin_a) * inverse_p(P_t_coin_c_given_t_coin_a)
+  denom = num + P_h_coin_a * inverse_p(P_h_coin_b_given_h_coin_a) + inverse_p(P_h_coin_a) * P_t_coin_c_given_t_coin_a
 
   num / denom
 end
