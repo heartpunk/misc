c467055fb0f40fefd95a765fd6754cc7f092f7f4
Ezekiel Smithburg <tehgeekmeister@gmail.com>
2015-10-05 22:18:05 -0400

    major refactor. pairwise collision detection implemented.

--- recurse/thermodynamics.elm
+++ recurse/thermodynamics.elm
@@ -2,36 +2,33 @@ import Color exposing (..)
 import Graphics.Collage exposing (..)
 import Graphics.Element exposing (..)
 import Time exposing (..)
+import List exposing (map)
 
-type alias Model =
-  { x  : Float
-  , y  : Float
-  , vx : Float
-  , vy : Float
+
+-- WHAT
+zip : List a -> List b -> List (a,b)
+zip xs ys =
+  case (xs, ys) of
+    ( x :: xs', y :: ys' ) ->
+        (x,y) :: zip xs' ys'
+
+    (_, _) ->
+        []
+
+
+type alias Model = List DiscRecord
+
+type alias DiscRecord =
+  { x        : Float
+  , y        : Float
+  , vx       : Float
+  , vy       : Float
+  , collided : Bool
   }
 
-main : Signal Element
-main =
-  Signal.map view (Signal.foldp update defaultDisc (fps 120))
-  
---main2 =
---  let
---    timeSoFar = Signal.foldp (+) 0 (fps 60)
---  in
---    Signal.map setting timeSoFar
-
-view : Model -> Element
-view discModel =
-  let
-    x = discModel.x
-    y = discModel.y
-    renderedDisc = move (x, y) disc
-  in
-    setting renderedDisc
-
 collageSize = 500
-discSize = 5
-discRadius = (discSize / 2) + 3
+discSize = 10
+discRadius = (discSize / 2) + 5
 upperBound = collageSize / 2
 lowerBound = -upperBound
 
@@ -39,30 +36,81 @@ update : Float -> Model -> Model
 update dt model =
   let truncateToBounds upper lower a = if a > upper then upper else (if a < lower then lower else a)
       collided x vx = (x > (upperBound - discRadius)) || (x < (lowerBound))
-      newPos x vx = if (collided x vx) then (truncateToBounds (upperBound - discRadius) (lowerBound + discRadius) x) else x + vx
-      newVel x vx = if (collided x vx) then -vx else vx
-  in
-    { model |
-      x  <- newPos model.x model.vx,
-      y  <- newPos model.y model.vy,
-      vx <- newVel model.x model.vx,
-      vy <- newVel model.y model.vy
-    }
+      newPos x vx   = if (collided x vx) then (truncateToBounds (upperBound - discRadius) (lowerBound + discRadius) x) else x + vx
+      newVel x vx   = if (collided x vx) then -vx else vx
+      currentCollidedPairs = collidedPairs (pairs model)
+      discCollided discRecord =
+        not (List.isEmpty (
+          List.filter
+            (\(a,b) -> (a == discRecord) || (b == discRecord))
+            currentCollidedPairs
+        ))
+      updatedRecord discRecord =
+        { discRecord |
+          x        <- newPos discRecord.x discRecord.vx,
+          y        <- newPos discRecord.y discRecord.vy,
+          vx       <- newVel discRecord.x discRecord.vx,
+          vy       <- newVel discRecord.y discRecord.vy,
+          collided <- discCollided discRecord
+        }
+  in map updatedRecord model
 
-disc =
+disc color =
   group
-    [ filled lightGrey (circle discSize)
+    [ filled color (circle discSize)
     , outlined (solid grey) (circle discSize)
       ]
 
-setting renderedDisc =
-  collage 500 500 [renderedDisc, outlined (solid red) (rect 500 500)]
 
-defaultDisc : Model
+-- this assumes a list of unique items.
+pairs : List a -> List (a,a)
+pairs items =
+  let
+    core (x::xs) pairsSoFar =
+      if   xs == []
+      then pairsSoFar
+      else core xs ( (map ( \y-> (x,y) ) xs) ++ pairsSoFar)
+  in
+    core items []
+
+collidedPairs : List (DiscRecord, DiscRecord) -> List (DiscRecord, DiscRecord)
+collidedPairs =
+  let -- don't do square root
+    distance a b       = sqrt((a.x - b.x)^2 + (a.y-b.y)^2)
+    pairCollided (a,b) = (distance a b) < ((discRadius * 2))
+  in
+    List.filter pairCollided
+
+view : Model -> Element
+view model =
+  let
+    color collided = if collided then red else lightGrey
+    renderedDisc discRecord = move (discRecord.x, discRecord.y) (disc (color discRecord.collided))
+  in
+    collage 500 500 ( (map renderedDisc model) ++ [outlined (solid red) (rect 500 500)])
+
+defaultDisc : DiscRecord
 defaultDisc =
-  { x  = 125
-  , y  = 125
-  , vx = 1
-  , vy = -1
+  { x        = 0
+  , y        = 0
+  , vx       = 2
+  , vy       = -1
+  , collided = False
   }
 
+defaultModel : Model
+defaultModel =
+  let
+    offsetDisc i =
+      { defaultDisc |
+        x <- i * 20 + defaultDisc.x,
+        y <- i * 20 + defaultDisc.y,
+        vx <- defaultDisc.vx + (0.2*i),
+        vy <- defaultDisc.vy - (0.3*i)
+      }
+  in
+    map offsetDisc [1..10]
+
+main : Signal Element
+main =
+  Signal.map view (Signal.foldp update defaultModel (fps 120))
